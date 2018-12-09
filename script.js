99797b1beab979b9772e20e6944e7c5c35872920
James Martinez <jamesmartinez@Jamess-Air.attlocal.net>
2018-12-09 16:00:24 -0600

    slowly getting it going

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ let nameInfo = () => {
   document.getElementById('fullName').innerHTML = fullName + '  ' +  welcome;
 
 }
-
   var button = document.getElementById('btn');
   button.addEventListener('click',nameInfo);
+
+let languageJs = () => {
+      var x = document.createElement("A");
+      var t = document.createTextNode("javaScript");
+      x.setAttribute("href", "https://github.com/JLMartinez1980/finalproject-2");
+      x.appendChild(t);
+      document.body.appendChild(x);
+  }
+
+  let buttonJs = document.getElementById('btnjs');
+  buttonJs.addEventListener('click', languageJs);
+
+  let languageRe = () => {
+        var a = document.createElement("A");
+        var b = document.createTextNode("React");
+        a.setAttribute("href", "https://github.com/JLMartinez1980/react-assignment-boilerplate");
+        a.appendChild(b);
+        document.body.appendChild(a);
+    }
+
+    let buttonRe = document.getElementById('btnre');
+    buttonRe.addEventListener('click', languageRe);
+
+    let languageAn = () => {
+          var c = document.createElement("A");
+          var d = document.createTextNode("Angular");
+          c.setAttribute("href", "https://github.com/JLMartinez1980/angular-final");
+          c.appendChild(d);
+          document.body.appendChild(c);
+      }
+
+      let buttonAn = document.getElementById('btnan');
+      buttonAn.addEventListener('click', languageAn);
